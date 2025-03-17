52b95f7c1b3af867cf79c03ccb27c36524393b17
Jamison-H-Fox <fox.jamison@outlook.com>
2025-03-16 18:34:43 -0600

    wip

--- src/db/migrations/002_create_players_days_tasks_tables.sql
+++ src/db/migrations/002_create_players_days_tasks_tables.sql
@@ -9,6 +9,8 @@ CREATE TABLE days (
   id TEXT PRIMARY KEY,
   date TEXT NOT NULL,
   day_of_week TEXT NOT NULL,
+  user_id TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
   CHECK (day_of_week IN ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'))
 );
 
@@ -17,10 +19,12 @@ CREATE TABLE tasks (
   name TEXT NOT NULL,
   player_id TEXT NOT NULL,
   day_id TEXT NOT NULL,
+  user_id TEXT NOT NULL,
   type TEXT NOT NULL,
   status TEXT NOT NULL,
   FOREIGN KEY (player_id) REFERENCES players(id),
   FOREIGN KEY (day_id) REFERENCES days(id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
   CHECK (type IN ('daily', 'weekly')),
   CHECK (status IN ('pending', 'pass', 'missed', 'completed'))
 );
--- src/db/migrations/003_add_user_id_to_tasks_and_days.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE days ADD COLUMN user_id TEXT NOT NULL DEFAULT 'default_user_id';
-ALTER TABLE tasks ADD COLUMN user_id TEXT NOT NULL DEFAULT 'default_user_id';
