a85b9b1e6720ad8f1d28f89e1d17d3ef2cc53f58
Jamison-H-Fox <fox.jamison@outlook.com>
2025-03-22 21:28:07 -0600

    added update functionality for players

--- src/modules/players/index.js
+++ src/modules/players/index.js
@@ -1,17 +1,62 @@
 import db from '../../db/db.js'
 import { v4 as uuidv4 } from 'uuid'
+import _ from 'lodash'
 
-export async function createPlayer(name, age, gender, user_id) {
+export async function createPlayer(name, age, gender, userId) {
   const playerId = uuidv4()
 
   const createPlayerQuery =
     'INSERT INTO players (id, name, age, gender, user_id) VALUES (?, ?, ?, ?, ?)'
   const createPlayer = db.prepare(createPlayerQuery)
-  await createPlayer.run(playerId, name, age, gender, user_id)
+  await createPlayer.run(playerId, name, age, gender, userId)
 
   const getPlayerQuery = 'SELECT name, age, gender FROM players WHERE id = ?'
   const getPlayer = db.prepare(getPlayerQuery)
   const player = await getPlayer.get(playerId)
 
   return player
+}
+
+export async function getAllPlayers(userId) {
+  const getPlayersQuery =
+    'SELECT id, name, age, gender from players WHERE user_id = ?'
+  const getPlayers = db.prepare(getPlayersQuery)
+  const players = await getPlayers.all(userId)
+
+  return players
+}
+
+export async function getPlayer(playerId) {
+  const getPlayerQuery = 'SELECT * FROM players WHERE id = ?'
+  const getPlayer = db.prepare(getPlayerQuery)
+  const player = await getPlayer.get(playerId)
+
+  if (!player) {
+    throw new Error(`Player with id: ${playerId} not found`)
+  }
+
+  return player
+}
+
+export async function updatePlayer(playerId, updates) {
+  const player = await getPlayer(playerId)
+  const filteredUpdates = _.omitBy(updates, _.isUndefined)
+  const updatedValues = { ...player, ...filteredUpdates }
+
+  const updatePlayerQuery = `UPDATE players SET name = ?,
+                         age = ?,
+                         gender = ?
+                         WHERE id = ?`
+
+  const updatePlayer = db.prepare(updatePlayerQuery)
+  await updatePlayer.run(
+    updatedValues.name,
+    updatedValues.age,
+    updatedValues.gender,
+    playerId,
+  )
+
+  const updatedPlayer = await getPlayer(playerId)
+
+  return updatedPlayer
 }
--- src/modules/players/routes.js
+++ src/modules/players/routes.js
@@ -1,25 +1,52 @@
-import { createPlayer } from './index.js'
+import { createPlayer, getAllPlayers, updatePlayer } from './index.js'
 import { authenticate } from '../../utlis/auth.js'
 
 export const playerRoutes = {
   '/players': {
     POST: async (req) => {
+      // I feel like I'm repeting this try/catch block a lot
+      // I wonder if I can handle this further up???
       try {
         const decoded = await authenticate(req)
+        const userId = decoded.id
         const { name, age, gender } = await req.json()
-        const user_id = decoded.id
 
-        const newPlayer = await createPlayer(name, age, gender, user_id)
+        const newPlayer = await createPlayer(name, age, gender, userId)
         return Response.json(newPlayer)
       } catch (err) {
         return new Response(err.message, { status: 401 })
       }
     },
-    GET: async () => {
-      return Response.json({ message: 'GET request to /players' })
+    GET: async (req) => {
+      try {
+        const decoded = await authenticate(req)
+        const userId = decoded.id
+
+        const players = await getAllPlayers(userId)
+        return Response.json(players)
+      } catch (err) {
+        return new Response(err.message, { status: 401 })
+      }
     },
-    PUT: async () => {
-      return Response.json({ message: 'PUT request to /players' })
+    PUT: async (req) => {
+      try {
+        await authenticate(req)
+        const { playerId, name, age, gender } = await req.json()
+        console.log('\n---\nplayerId:', playerId)
+        console.log('name:', name)
+        console.log('age:', age)
+        console.log('gender:', gender, '\n---\n')
+
+        const updatedPlayer = await updatePlayer(playerId, {
+          name,
+          age,
+          gender,
+        })
+
+        return Response.json(updatedPlayer)
+      } catch (err) {
+        return new Response(err.message, { status: 401 })
+      }
     },
     DELETE: async () => {
       return Response.json({ message: 'DELETE request to /players' })
